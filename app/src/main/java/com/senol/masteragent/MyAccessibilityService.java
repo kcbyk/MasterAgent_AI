285f7ee37859e73254ea95fa1619b7456d2645e4
kcbyk <senolkoocabiyik@gmail.com>
2026-02-26 07:49:45 +0300

    Erisilebilirlik servisi eklendi

---
+++ app/src/main/java/com/senol/masteragent/MyAccessibilityService.java
@@ -0,0 +1,7 @@
+package com.senol.masteragent;
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+public class MyAccessibilityService extends AccessibilityService {
+    @Override public void onAccessibilityEvent(AccessibilityEvent event) {}
+    @Override public void onInterrupt() {}
+}
